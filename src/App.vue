66a97234df698d502d303ad5a03483aa31cde2d7
Célio Viana da Silva Júnior <celio_cjr@hotmail.com>
2021-09-12 14:51:52 -0300

    vue bind data

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
     <div id="app">
-        <p :class="comprou ? 'verde' : 'vermelho'">Comprou:</p>
-    </div>
-        
+        <div>
+            <h1>
+            {{ apple.companyName }}
+            </h1>
+            <p :class="apple.marketCap > google.marketCap ? 'verde' : 'vermelho' ">{{ apple.marketCap}}</p>
+        </div>
+        <div>
+            <h1>
+            {{ google.companyName }}
+            </h1>
+            <p :class="google.marketCap > apple.marketCap ? 'verde' : 'vermelho' ">{{ google.marketCap }}</p>
+        </div>
+        <button @click="pushData">Push</button>
+    </div>     
 </template>
 
 <script>
@@ -12,7 +23,28 @@ export default {
     name: '#app',
     data() {
         return {
-            comprou: true
+            google: {},
+            apple: {},
+        }
+    },
+    methods: {
+        fetchGoogle() {
+            fetch("https://api.origamid.dev/stock/googl/quote")
+            .then(r => r.json())
+            .then(google => {
+                this.google = google
+            })
+        },
+        fetchApple() {
+            fetch("https://api.origamid.dev/stock/aapl/quote")
+            .then(r => r.json())
+            .then(apple => {
+                this.apple = apple
+            })
+        },
+        pushData() {
+            this.fetchGoogle();
+            this.fetchApple()
         }
     }
 
@@ -22,5 +54,11 @@ export default {
 </script>
 
 <style>
+    .verde {
+        color: green
+    }
 
+    .vermelho {
+        color: red
+    }
 </style>
