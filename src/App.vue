08091be68404fbdfe7a29895ab356637edc42edd
Célio Viana da Silva Júnior <celio_cjr@hotmail.com>
2021-09-11 23:00:55 -0300

    initial commit vue cli

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="app">
+        {{ message }}
+        <HeaderPrincipal />
+    </div>
+</template>
+
+<script>
+
+import HeaderPrincipal from './components/HeaderPrincipal.vue'
+
+export default {
+    components: {
+        HeaderPrincipal
+    },
+    data() {
+        return {
+            message: "This is a message."
+        }
+    }
+
+}
+
+
+</script>
+
+<style>
+
+</style>
